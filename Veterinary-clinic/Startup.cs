64dbf5bad152e87190f98c40d3b3741bd1f1c716
liberomgm <vadick.n2018@yandex.ru>
2022-06-17 19:22:55 +0300

    Connection to PostrgreSQL

--- Veterinary-clinic/Startup.cs
+++ Veterinary-clinic/Startup.cs
@@ -28,11 +28,13 @@ namespace Veterinary_clinic
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
+
             services.AddRazorPages();
         }
 
